3c869875d44e392a84b5f8673a1716c56112a239
sr <candy871121@gmail.com>
2020-05-18 11:59:23 +0800

    未完成

--- app/src/main/java/com/u1065138/intentbundleapp/MainActivity.java
+++ app/src/main/java/com/u1065138/intentbundleapp/MainActivity.java
@@ -1,15 +1,36 @@
 package com.u1065138.intentbundleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtHeight, edtWeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        edtHeight = findViewById(R.id.edtHeight);
+        edtWeight = findViewById(R.id.edtWeight);
+
     }
+    public void btnBMI(View view) {
+
+       Intent intent = new Intent();
+       intent.setClass(MainActivity.this,Bmi2Activity.class);
+
+       Bundle bundle =new Bundle();
+       bundle.putString("HEIGHT",edtHeight.getText().toString());
+       bundle.putString("WEIGHT",edtWeight.getText().toString());
+       intent.putExtras(bundle);
+       startActivity(intent);
+       
     }
+}
+
 
