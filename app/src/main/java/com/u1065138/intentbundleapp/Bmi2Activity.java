a0d5ad55b89e96f38fa81fea71bf18939d1b21ca
sr <candy871121@gmail.com>
2020-05-18 11:07:50 +0800

    first commit

---
+++ app/src/main/java/com/u1065138/intentbundleapp/Bmi2Activity.java
@@ -0,0 +1,15 @@
+package com.u1065138.intentbundleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Bmi2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmi2);
+
+    }
+}
